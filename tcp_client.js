a47dcbbf2e2f6d7532ea5136e69a368f16a9be34
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-24 23:54:29 +0530

    Developed code to write data on client-socket and destroy socket after it.

--- tcp_client.js
+++ tcp_client.js
@@ -1,5 +1,6 @@
 const NET = require('net');
 const SOCKET_UTIL = require('./socket_util');
+var countWriteOperation = 0;
 
 var clientSocket = NET.connect({
 	port: 3091, host: '127.0.0.1', allowHalfOpen: true
@@ -22,8 +23,27 @@ clientSocket.on("data", function (data) {
 	SOCKET_UTIL.socketAddress(clientSocket);
 
 	console.log('Data from server : ' + data + ' | #20000');
-	clientSocket.destroy();
+	/**
+	 * How we can destroy connected socket.
+	 * clientSocket.destroy();
+	 */
 });
+
+/**
+ * Here we'll write using socket 3 times, with time interval 2 seconds and later
+ * will destroy socket.
+ */
+setInterval(function () {
+
+	if (countWriteOperation == 3) {
+
+		clientSocket.destroy();
+	}
+
+	++countWriteOperation;
+	clientSocket.write('Writing data to server....');
+
+}, 2000);
 
 /**
  * Socket `close` event handler.
@@ -32,3 +52,4 @@ clientSocket.on("close", function (data) {
 
 	console.log('Client socket close event occured | #20001');
 });
+
