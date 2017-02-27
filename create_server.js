cf5ebd5b549e8d2205b91d04ccbbbf933714c99e
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-27 23:35:07 +0530

    Developed code to implment basic for nodejs http module.

--- create_server.js
+++ create_server.js
@@ -1,21 +1,41 @@
+'use strict';
+
 const HTTP = require('http');
+const URL = require("url");
 
 /**
  * `httpFn_CB` is a callback function, get execute after server creation.
  */
 function httpFn_CB(request, response) {
 
+	var body;
+	var urlObj;
+
+	body = "Server has been created | #10003";
+
+	urlObj = URL.parse(request.url);
+	console.log(JSON.stringify(urlObj));
+
+	/**
+	 *	we may redirect user to another site using status code 302
+	 * "Location": "https://www.google.co.in", with status code 302
+	 */
 	response.writeHead(200, {
-		"Content-Type": "text/plain"
+		"Content-Type": "text/plain",
+
+		"Content-Length": body.length
 	});
 
-	console.log('Server has been created');
-	response.end('Server has been created');
-
+	response.write(body);
+	response.end();
 }
+
+/**
+ * Creating server instance.
+ */
 var proxy = HTTP.createServer(httpFn_CB);
 
-proxy.listen(3090, '127.0.0.1', () => {
+proxy.listen(3091, '127.0.0.1', () => {
 
-	console.log('Server is listening on port 3090');
+	console.log('Server is listening on port 3091 | #10001');
 });
