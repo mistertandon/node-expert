35e2dbd9a27e67c461f7e4f73e693ee845578049
Mister Tandon <enggparveshtandon@gmail.com>
2017-02-27 20:13:19 +0530

    Developed code to demonstrate how we can configure max event listeners. How we get listeners array associated with event.

--- events_pipe.js
+++ events_pipe.js
@@ -8,6 +8,10 @@ netServer = NET.createServer();
 netServer.on('connection', function (socket) {
 
 	/**
+	 * Configure number of listeners we can attached with event.
+	 */
+	socket.setMaxListeners(2);
+	/**
 	 * Pipe event on Socket.
 	 */
 	socket.on('pipe', function () {
@@ -29,6 +33,22 @@ netServer.on('connection', function (socket) {
 			socket.unpipe(socket);
 		}, 10000);
 	});
+
+	/**
+	 * Getting all listeners associated with pipe event.
+	 */
+	var pipeEventListeners;
+
+	pipeEventListeners = socket.listeners('pipe');
+	console.log('=========== First listener associated with `pipe` event ===========\n\n');
+	console.log(pipeEventListeners[0].toString());
+
+	console.log('=========== Second listener associated with `pipe` event ===========\n\n');
+	console.log(pipeEventListeners[1].toString());
+
+	console.log('============================================\n\n');
+	console.log('Listeners length associated with pipe event : ' + socket.listeners('pipe').length);
+	console.log('\n\n============================================');
 
 	socket.pipe(socket);
 
